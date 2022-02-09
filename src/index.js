9dd510c1c0e2d0abdf2aba34cb9deed6eff07428
Betelihem <Betelihem>
2022-02-09 11:30:59 -0800

    1st requirenment done: Renders All Images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // write your code here
+const url = 'http://localhost:3000/ramens'
+
+// DOM selector && Created Elements
+list = document.querySelector('#ramen-menu')
+
+// Register Event Listners
+
+
+// Fetches
+function fetchAllRamen() {
+    return fetch (url)
+    .then (res => res.json())
+}
+
+
+// Rendering
+function renderAllRamen(ramenArr) {
+    ramenArr.forEach(renderOneRamen)
+}
+
+function renderOneRamen(ramenObj) {
+    let img = document.createElement('img')
+    img.src = ramenObj.image
+    img.classList.add('img')
+    list.appendChild(img)
+
+}
+
+
+// // Event Handler
+
+
+
+// // Initializer(s)
+fetchAllRamen().then(renderAllRamen)
