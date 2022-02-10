7f87542fe4a3e3430456e4debe27c4795df73653
Betelihem <Betelihem>
2022-02-09 16:35:44 -0800

    Core Deliverables Met

--- src/index.js
+++ src/index.js
@@ -1,36 +1,71 @@
-// write your code here
-const url = 'http://localhost:3000/ramens'
+// Global Variables
+const baseURL = 'http://localhost:3000'
 
-// DOM selector && Created Elements
-list = document.querySelector('#ramen-menu')
+// DOM Selectors
+const menu = document.querySelector('#ramen-menu')
+const detail = document.querySelector('#ramen-detail')
+const rating = document.querySelector('#rating-display')
+const comment = document.querySelector('#comment-display')
+const form = document.querySelector('#new-ramen')
 
-// Register Event Listners
+// Listeners
+form .addEventListener('submit', handleAddRamen)
 
-
-// Fetches
-function fetchAllRamen() {
-    return fetch (url)
-    .then (res => res.json())
+// Fetchers
+function getAllRamens() {
+    return fetch(baseURL + '/ramens')
+    .then (r => r.json())
 }
 
-
-// Rendering
+// Render Functions
 function renderAllRamen(ramenArr) {
-    ramenArr.forEach(renderOneRamen)
+    ramenArr.forEach(renderOneMenu)
 }
 
-function renderOneRamen(ramenObj) {
-    let img = document.createElement('img')
+function renderOneMenu(ramenObj) {
+    // console.log('ramenObj: ', ramenObj)
+    const img = document.createElement('img')
+
     img.src = ramenObj.image
-    img.classList.add('img')
-    list.appendChild(img)
 
+    img.addEventListener('click', () => renderDetail(ramenObj))
+
+    menu.appendChild(img)
 }
 
+function renderDetail(ramObj) {
+    console.log('ramObj: ', ramObj)
+    detail.innerHTML = `
+        <img class="detail-image" src=${ramObj.image} alt=${ramObj.name} />
+        <h2 class="name">${ramObj.name}</h2>
+        <h3 class="restaurant">${ramObj.restaurant}</h3>
+    `
 
-// // Event Handler
+    rating.textContent = ramObj.rating
+    comment.textContent = ramObj.comment
+}
 
+// Event Handlers
+function handleAddRamen(e) {
+    e.preventDefault()
+    console.log('e: ', e)
+    const name = e.target.name.value
+    const restaurant = e.target.restaurant.value
+    const image = e.target.image.value
+    const rating = e.target.rating.value
+    const comment = e.target['new-comment'].value
+    const newRamObj = {
+        name,
+        restaurant,
+        image,
+        rating,
+        comment
+    }
+    renderOneMenu(newRamObj)
+    // form.reset()
+    e.target.reset()
+}
 
-
-// // Initializer(s)
-fetchAllRamen().then(renderAllRamen)
+// Intializers
+// getAllRamens().then(ramenArr => renderAllRamens(ramenArr))
+getAllRamens().then(renderAllRamen)
